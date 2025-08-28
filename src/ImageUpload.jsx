c79fd9a26852cebd8b71d3af07cdc41bfb611475
Maulik-khambhdiya <maulikkhambhdiya444@gmail.com>
2025-08-28 19:26:19 +0530

    react-crud

--- src/ImageUpload.jsx
+++ src/ImageUpload.jsx
@@ -1,22 +1,19 @@
-
 import axios from 'axios'
 import { Field, Form, Formik } from 'formik'
 import React, { useState } from 'react'
 
 const ImageUpload = () => {
-
-    const [ini, setIni] = useState({
+    const [preview, setPreview] = useState(null); // To show image preview
+    const initialValues = {
         name: '',
-        image: ''
-    })
-
+        image: null
+    }
     const handleSubmit = (values, { resetForm }) => {
-        // console.log(values);
-        // let formData = new FormData()
-        // formData.append("name", values.name)
-        // formData.append("image", values.image)
+        let formData = new FormData();
+        formData.append("name", values.name);
+        formData.append("image", values.image);
 
-        axios.post('https://generateapi.techsnack.online/api/user', values, {
+        axios.post('https://generateapi.techsnack.online/api/user', formData, {
             headers: {
                 Authorization: 'IpcMaiQUcVTvsato',
                 'Content-Type': 'multipart/form-data',
@@ -24,25 +21,46 @@ const ImageUpload = () => {
         })
             .then(() => {
                 console.log("Success");
+                resetForm();         // Clear the form
+                setPreview(null);    // Clear preview
             })
             .catch((error) => {
                 console.log(error);
-            })
-
+            });
     }
     return (
         <div>
             <Formik
-                initialValues={ini}
+                initialValues={initialValues}
                 onSubmit={handleSubmit}
             >
                 {
                     ({ setFieldValue }) => (
                         <Form encType='multipart/form-data'>
-                            <Field name="name"></Field> <br /><br />
-                            <input type="file" name="image" id="" onChange={(e) => setFieldValue("image", e.target.files[0])
-                            } />
-                            <button>Submit</button>
+                            <label>Name:</label><br />
+                            <Field name="name" /><br /><br />
+                            <input
+                                type="file"
+                                name="image"
+                                accept="image/*"
+                                onChange={(e) => {
+                                    const file = e.target.files[0];
+                                    setFieldValue("image", file);
+                                    if (file) {
+                                        setPreview(URL.createObjectURL(file));
+                                    } else {
+                                        setPreview(null);
+                                    }
+                                }}
+                            /><br /><br />
+                            {preview && (
+                                <div>
+                                    <img src={preview} alt="Preview" width="200" />
+                                    <br /><br />
+                                </div>
+                            )}
+
+                            <button type="submit">Submit</button>
                         </Form>
                     )
                 }
@@ -51,4 +69,4 @@ const ImageUpload = () => {
     )
 }
 
-export default ImageUpload
+export default ImageUpload;
